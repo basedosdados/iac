e35fb1c8c64ef9b36440bb0bf37c8ecf8d6d05ed
Guilherme Peres <contact@gperes.dev>
2022-08-01 14:28:24 -0300

    chore[ci-terraform]: update .tfvars values

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,21 +5,27 @@
 ############
 
 variable "project_id" {
+  type        = string
   description = "The GCP project ID to use for the cluster."
+  default     = "basedosdados-dev"
 }
 
 variable "bucket_name" {
+  type        = string
   description = "Name of the GCS bucket for storing Terraform data"
+  default     = "terraform-data-basedosdados-dev"
 }
 
 variable "region" {
-  default     = "us-central1"
+  type        = string
   description = "The GCP region to use for the cluster."
+  default     = "us-central1"
 }
 
 variable "zone" {
-  default     = "us-central1-c"
+  type        = string
   description = "The GCP zone to use for the cluster."
+  default     = "us-central1-c"
 }
 
 ############
@@ -29,83 +35,100 @@ variable "zone" {
 ############
 
 variable "sql_version" {
-  default     = "POSTGRES_13"
+  type        = string
   description = "SQL Version"
+  default     = "POSTGRES_13"
 }
 
 variable "sql_instance_tier" {
-  default     = "db-f1-micro"
+  type        = string
   description = "SQL Instance Tier"
+  default     = "db-f1-micro"
 }
 
 variable "sql_disk_size" {
-  default     = "80"
+  type        = number
   description = "SQL Disk Size"
+  default     = 80
 }
 
 variable "sql_disk_autoresize" {
-  default     = true
+  type        = bool
   description = "SQL Disk Auto Resize"
+  default     = true
 }
 
 variable "sql_backup_enabled" {
-  default     = true
+  type        = bool
   description = "SQL Backup Enabled"
+  default     = true
 }
 
 variable "sql_ckan_staging_user_name" {
-  default     = "ckan_staging"
+  type        = string
   description = "SQL CKAN-Staging User Name"
+  default     = "ckan_staging"
 }
 
 variable "sql_ckan_staging_user_password" {
+  type        = string
   description = "SQL CKAN-Staging User Password"
 }
 
 variable "sql_ckan_staging_db_name" {
-  default     = "ckan_staging"
+  type        = string
   description = "SQL CKAN-Staging DB Name"
+  default     = "ckan_staging"
 }
 
 variable "sql_ckan_production_user_name" {
-  default     = "ckan_production"
+  type        = string
   description = "SQL CKAN-Production User Name"
+  default     = "ckan_production"
 }
 
 variable "sql_ckan_production_user_password" {
+  type        = string
   description = "SQL CKAN-Production User Password"
 }
 
 variable "sql_ckan_production_db_name" {
-  default     = "ckan_production"
+  type        = string
   description = "SQL CKAN-Production DB Name"
+  default     = "ckan_production"
 }
 
 variable "sql_metabase_user_name" {
-  default     = "metabase"
+  type        = string
   description = "SQL Metabase User Name"
+  default     = "metabase"
 }
 variable "sql_metabase_user_password" {
+  type        = string
   description = "SQL Metabase User Password"
 }
 
 variable "sql_metabase_db_name" {
-  default     = "metabase"
+  type        = string
   description = "SQL Metabase DB Name"
+  default     = "metabase"
 }
 
 variable "sql_prefect_user_name" {
-  default     = "prefect"
+  type        = string
   description = "SQL Prefect User Name"
+  default     = "prefect"
 }
 
 variable "sql_prefect_user_password" {
+  type        = string
   description = "SQL Prefect User Password"
 }
 
 variable "sql_prefect_db_name" {
-  default     = "prefect"
+  type        = string
   description = "SQL Prefect DB Name"
+  default     = "prefect"
 }
 
 ############
@@ -115,36 +138,43 @@ variable "sql_prefect_db_name" {
 ############
 
 variable "static_pool_machine_type" {
+  type        = string
   description = "The machine type to use for the cluster's static pool."
   default     = "n1-standard-4"
 }
 
 variable "static_pool_node_count" {
+  type        = number
   description = "The number of nodes to use for the static pool."
   default     = 2
 }
 
 variable "dynamic_pool_machine_type" {
+  type        = string
   description = "The machine type to use for the cluster's dynamic pool."
   default     = "e2-medium"
 }
 
 variable "dynamic_pool_node_count" {
+  type        = number
   description = "The default number of nodes to use for the dynamic pool."
   default     = 0
 }
 
 variable "dynamic_pool_node_min" {
+  type        = number
   description = "The minimum number of nodes to use for the dynamic pool."
   default     = 0
 }
 
 variable "dynamic_pool_node_max" {
+  type        = number
   description = "The maximum number of nodes to use for the dynamic pool."
   default     = 1
 }
 
 variable "dynamic_pool_node_preemptible" {
+  type        = bool
   description = "Whether to use preemptible nodes for the dynamic pool."
   default     = true
 }
@@ -155,11 +185,13 @@ variable "dynamic_pool_node_preemptible" {
 #
 ############
 variable "gsa_cloudsql_account_id" {
+  type        = string
   description = "Cloud SQL Account ID"
   default     = "gsa-cloudsql"
 }
 
 variable "role_iam_workload_identity_user" {
+  type        = string
   description = "IAM workload identity user role."
   default     = "roles/iam.workloadIdentityUser"
 }
