951eb5d5edbc3daab6e652079b6943524c98de29
Vinicius <vncsna@gmail.com>
2023-10-04 19:58:43 -0300

    feat: add website public bucket

--- terraform/cloud_storage/main.tf
+++ terraform/cloud_storage/main.tf
@@ -13,3 +13,24 @@ resource "google_storage_bucket" "website_images" {
     enabled = var.bucket_versioning
   }
 }
+
+resource "google_storage_bucket" "website" {
+  name                        = "basedosdados-website"
+  location                    = var.bucket_location
+  storage_class               = var.bucket_storage_class
+  force_destroy               = var.bucket_force_destroy
+  uniform_bucket_level_access = var.bucket_uniform_bucket_level_access
+
+  versioning {
+    enabled = var.bucket_versioning
+  }
+}
+
+resource "google_storage_bucket_iam_binding" "all_users_read" {
+  bucket = google_storage_bucket.website.name
+  role   = "roles/storage.objectViewer"
+
+  members = [
+    "allUsers",
+  ]
+}
